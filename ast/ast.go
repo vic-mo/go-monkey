8f2716613a31226cb3062a308de441cdbd38f7f5
Victor Morel <victor.morel@spendesk.com>
2024-10-13 11:08:21 +0200

    feat: implement parser

--- ast/ast.go
+++ ast/ast.go
@@ -3,6 +3,7 @@ package ast
 import (
 	"bytes"
 	"monkey/token"
+	"strings"
 )
 
 type Node interface {
@@ -50,6 +51,15 @@ type Identifier struct {
 func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
 func (i *Identifier) String() string       { return i.Value }
+
+type IntegerLiteral struct {
+	Token token.Token
+	Value int64
+}
+
+func (i *IntegerLiteral) expressionNode()      {}
+func (i *IntegerLiteral) TokenLiteral() string { return i.Token.Literal }
+func (i *IntegerLiteral) String() string       { return i.Token.Literal }
 
 type LetStatement struct {
 	Token token.Token
@@ -117,4 +127,145 @@ func (ls *ExpressionStatement) String() string {
 
 	return ""
 
+}
+
+type PrefixExpression struct {
+	Token    token.Token
+	Operator string
+	Right    Expression
+}
+
+func (pe *PrefixExpression) expressionNode()      {}
+func (pe *PrefixExpression) TokenLiteral() string { return pe.Token.Literal }
+func (pe *PrefixExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("(")
+	out.WriteString(pe.Operator)
+	out.WriteString(pe.Right.String())
+	out.WriteString(")")
+
+	return out.String()
+}
+
+type InfixExpression struct {
+	Token    token.Token
+	Operator string
+	Right    Expression
+	Left     Expression
+}
+
+func (pe *InfixExpression) expressionNode()      {}
+func (pe *InfixExpression) TokenLiteral() string { return pe.Token.Literal }
+func (pe *InfixExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("(")
+	out.WriteString(pe.Left.String())
+	out.WriteString(" " + pe.Operator + " ")
+	out.WriteString(pe.Right.String())
+	out.WriteString(")")
+
+	return out.String()
+}
+
+type Boolean struct {
+	Token token.Token
+	Value bool
+}
+
+func (pe *Boolean) expressionNode()      {}
+func (pe *Boolean) TokenLiteral() string { return pe.Token.Literal }
+func (pe *Boolean) String() string {
+	return pe.Token.Literal
+}
+
+type IfExpression struct {
+	Token       token.Token
+	Condition   Expression
+	Consequence *BlockStatement
+	Alternative *BlockStatement
+}
+
+func (pe *IfExpression) expressionNode()      {}
+func (pe *IfExpression) TokenLiteral() string { return pe.Token.Literal }
+func (pe *IfExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("if")
+	out.WriteString(pe.Condition.String())
+	out.WriteString(" ")
+	out.WriteString(pe.Consequence.String())
+	if pe.Alternative != nil {
+		out.WriteString("else")
+		out.WriteString(pe.Alternative.String())
+	}
+
+	return out.String()
+}
+
+type BlockStatement struct {
+	Token      token.Token
+	Statements []Statement
+}
+
+func (pe *BlockStatement) statementNode()       {}
+func (pe *BlockStatement) TokenLiteral() string { return pe.Token.Literal }
+func (pe *BlockStatement) String() string {
+	var out bytes.Buffer
+
+	for _, s := range pe.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
+
+type FunctionLiteral struct {
+	Token      token.Token
+	Parameters []*Identifier
+	Body       *BlockStatement
+}
+
+func (pe *FunctionLiteral) expressionNode()      {}
+func (pe *FunctionLiteral) TokenLiteral() string { return pe.Token.Literal }
+func (pe *FunctionLiteral) String() string {
+	var out bytes.Buffer
+
+	params := []string{}
+	for _, p := range pe.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString(pe.TokenLiteral())
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(")")
+	out.WriteString(pe.Body.String())
+
+	return out.String()
+}
+
+type CallExpression struct {
+	Token     token.Token
+	Function  Expression
+	Arguments []Expression
+}
+
+func (pe *CallExpression) expressionNode()      {}
+func (pe *CallExpression) TokenLiteral() string { return pe.Token.Literal }
+func (pe *CallExpression) String() string {
+	var out bytes.Buffer
+
+	args := []string{}
+	for _, p := range pe.Arguments {
+		args = append(args, p.String())
+	}
+
+	out.WriteString(pe.Function.String())
+	out.WriteString("(")
+	out.WriteString(strings.Join(args, ", "))
+	out.WriteString(")")
+
+	return out.String()
 }
